8a210ac9c599e16ca27e34cf805a38a01f954d6e
mzkamixo <167372860+mzkamixo@users.noreply.github.com>
2024-08-29 01:28:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const  primeiroNome = "Kamilli Vitorya";
+const sobrenome = "Borgmann de Oliveira";
+const idade = 16;
+const altura = 1.74;
+const massa = 50
+const cidadeNatal = "Joinville";
+
+console.log("O meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("e meu sobrenome é:");
+console.log(sobrenome);
+console.log("minha idade é:");
+console.log(idade);
+console.log("minha altura é:");
+console.log(altura);
+console.log("minha massa é:");
+console.log(massa);
+console.log("minha cidade natal é:");
+console.log(cidadeNatal);
